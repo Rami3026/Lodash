1e46162fa1a4a3fee8acbabaadc508ef845aed37
Rami3026 <91858420+Rami3026@users.noreply.github.com>
2021-11-07 19:35:35 +0000

    Add files via upload

---
+++ lodash.js
@@ -0,0 +1,21 @@
+const _ = {};
+ clamp(number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
+    const clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  }
+  .clamp = Math.max(lower,);
+  return clampedlower;
+  Math.min (upper);
+  return clampedlower, clampedupper;
+  inRange(number, start, end);
+  return false;
+  return true;
+};
+
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
